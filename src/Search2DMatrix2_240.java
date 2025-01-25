1540f371f2199f96a4ef43769d82a1b6cb76cd81
Shakti <shaktisarkar1999@gmail.com>
2025-01-25 18:44:12 +0530

    240 edit

--- src/Search2DMatrix2_240.java
+++ src/Search2DMatrix2_240.java
@@ -14,6 +14,9 @@ public class Search2DMatrix2_240 {
     }
 
     public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix == null || matrix.length < 1 || matrix[0].length <1) {
+            return false;
+        }
         int down = 0, right = matrix[0].length - 1;
         System.out.println("matrix.length => " + matrix.length);
         while (down < matrix.length && right >= 0) {
